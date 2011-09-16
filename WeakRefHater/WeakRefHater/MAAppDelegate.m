1c9a6fb9a5d1c9a391dd2db1990e1b39839547ec
joshaber <joshaber@gmail.com>
2011-09-16 14:56:10 -0700

    example project

---
+++ WeakRefHater/WeakRefHater/MAAppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  MAAppDelegate.m
+//  WeakRefHater
+//
+//  Created by Josh Abernathy on 9/16/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MAAppDelegate.h"
+
+@implementation MAAppDelegate
+
+
+#pragma mark NSApplicationDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification {
+	
+}
+
+
+#pragma mark API
+
+@synthesize window;
+
+@end
